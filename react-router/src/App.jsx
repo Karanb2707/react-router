2e3c60367169923e4f5645855430901627e70b60
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-24 21:27:25 +0530

    Create router

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,39 @@
+import { Link, NavLink, Route, Routes } from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
+import Shop from './components/Shop'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
 
 function App() {
 
   return (
-    <div className="text-center">
-      Hello
+    <div className="w-full text-center">
+
+      <nav className="w-full flex justify-center bg-pink-600 p-2 mb-5">
+        <ul className="flex space-x-4 text-white font-semibold">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </div>
   )
 }
