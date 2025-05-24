cfc3b25a4fed8269916d6f646d6411f4908a13e9
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-24 21:54:35 +0530

    useNavigate hook done

--- react-router/src/components/Shop.jsx
+++ react-router/src/components/Shop.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Shop = () => {
+  
+  const navigate = useNavigate();
+
+  function clickHandler() {
+    // move to about page
+    navigate('/about');
+  }
+
+  function backHandler() {
+    navigate(-1);
+  }
+
   return (
     <div>
-      Shop Page
+      <div>
+        Shop Page
+      </div>
+
+      <button className='py-1 px-2 border rounded-md mt-4 bg-red-500 text-white' onClick={clickHandler}>
+        About
+      </button>
+
+      <button className='py-1 px-2 border rounded-md m-4 bg-purple-800 text-white' onClick={backHandler}>
+        Back
+      </button>
+
     </div>
   )
 }
